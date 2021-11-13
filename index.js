d192f55927dc46bab1927a38a24f0eb6080bdee7
Lukas Vidfar <lgkvidfar@gmail.com>
2021-11-13 13:27:48 +0100

    1

--- index.js
+++ index.js
@@ -6,11 +6,22 @@ const mongoose = require('mongoose')
 const app = express()
 const Person = require('./models/person')
 
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+
 app.use(express.static('build'))
-app.use(cors())
 app.use(express.json())
+app.use(requestLogger)
+app.use(cors())
 morgan.token('body', req =>  JSON.stringify(req.body))
 app.use(morgan(':method :url :response-time ms :body'))
+
+
 
   app.get('/info', (request, response) => {
     Person.find({}).then(returned => {
@@ -20,19 +31,32 @@ app.use(morgan(':method :url :response-time ms :body'))
     <h3>${Date()} </h3>
     `)
     })
+    .catch(error => {
+      console.log("error info", error)
+      response.status(500).end()
+    })
   })
   
   app.get('/persons', (request, response) => {
     Person.find({}).then(returned => {
       response.json(returned)
     })
-  })
-
-  app.get('/persons/:id', (request, response) => {
-    Person.findById(request.params.id).then(person => {
-      response.json(person)
+    .catch(error => {
+      console.log("error /persons/", error)
+      response.status(500).end()
     })
   })
+
+  app.get('/persons/:id', (request, response, next) => {
+    Person.findById(request.params.id).then(person => {
+      if(person) {
+        response.json(person)
+      } else {
+        response.status(404).send({error: "id doesn't exist"}).end()
+      }
+    })
+    .catch(error => next(error))
+    })
 
   app.post('/persons', (request, response) => {
     const body = request.body
@@ -63,8 +87,23 @@ app.use(morgan(':method :url :response-time ms :body'))
     response.status(404).send({ error: 'unknown endpoint' })
   }
   app.use(unknownEndpoint)
+
+  const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+  
+    if (error.name === 'CastError') {
+      return response.status(400).send({ error: 'malformatted id' })
+    } 
+  
+    next(error)
+  }
+  
+  // this has to be the last loaded middleware.
+  app.use(errorHandler)
+
   
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
+
