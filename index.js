f8a80589cbcb0299d49334057e01c56e6d2c8188
Lukas Vidfar <lgkvidfar@gmail.com>
2021-11-13 11:56:30 +0100

    1

--- index.js
+++ index.js
@@ -1,34 +1,16 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const mongoose = require('mongoose')
 const app = express()
+const Person = require('./models/person')
 
 app.use(express.static('build'))
 app.use(cors())
 app.use(express.json())
 morgan.token('body', req =>  JSON.stringify(req.body))
 app.use(morgan(':method :url :response-time ms :body'))
-
-const url =
-  `mongodb+srv://fullstack:nioka20@cluster0.nkjni.mongodb.net/phonebookApp?retryWrites=true&w=majority`
-
-mongoose.connect(url)
-
-const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
-})
-
-personSchema.set('toJSON', {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
-  }
-})
-
-const Person = mongoose.model('Person', personSchema)
 
   app.get('/info', (request, response) => {
     Person.find({}).then(returned => {
@@ -47,47 +29,29 @@ const Person = mongoose.model('Person', personSchema)
   })
 
   app.get('/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(person => person.id === id)
-    if (person) {
-        response.json(person)
-      } else {
-        response.status(404).end()
-      }
+    Person.findById(request.params.id).then(person => {
+      response.json(person)
+    })
   })
-
-  const generateId = () => {
-    const maxId = persons.length > 0
-      ? Math.max(...persons.map(p => p.id))
-      : 0
-    return maxId + 1
-  }
 
   app.post('/persons', (request, response) => {
     const body = request.body
-    const nameList = persons.map(p => p.name)
-    const numberList = persons.map(p => p.number)
 
-
-    if (!body.name) {
+    if (body.name === undefined || body.number === undefined) {
         return response.status(400).json({ 
-          error: 'name missing' 
-        })
-      } else if (nameList.includes(body.name)) {
-        return response.status(400).json({
-            error: "duplicate name"
+          error: 'information missing' 
         })
       }
 
-  const person = {
+  const person = new Person({
     name: body.name,
-    number: body.number,
-    id: generateId(),
-  }
-  
-  persons = persons.concat(person)
-  response.json(person)
+    number: body.number
   })
+  
+  person.save().then(savedPerson => {
+    response.json(savedPerson)
+  })
+})
 
   app.delete('/persons/:id', (request, response) => {
     const id = Number(request.params.id)
@@ -98,10 +62,9 @@ const Person = mongoose.model('Person', personSchema)
   const unknownEndpoint = (request, response) => {
     response.status(404).send({ error: 'unknown endpoint' })
   }
-  
   app.use(unknownEndpoint)
   
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
