7725185634f92a75354619a6f310e7c843d33ec4
Lukas Vidfar <lgkvidfar@gmail.com>
2021-11-15 17:02:46 +0100

    all linting is done safeluy

--- mongo.js
+++ mongo.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose')
 
 if (process.argv.length < 3) {
-  console.log('Please provide the password as an argument: node mongo.js <password>')
-  process.exit(1)
+    console.log('Please provide the password as an argument: node mongo.js <password>')
+    process.exit(1)
 }
 
 const password = process.argv[2]
@@ -13,18 +13,18 @@ const url =
 mongoose.connect(url)
 
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
+    name: String,
+    number: String,
 })
 
 const Person = mongoose.model('Person', personSchema)
 
 Person.find({}).then(result => {
     result.forEach(person => {
-      console.log(person)
+        console.log(person)
     })
     mongoose.connection.close()
-  })
+})
 
 // const person = new Person({
 //     name: "third",
